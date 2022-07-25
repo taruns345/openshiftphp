15b56e3f6ef626745146a53153381b7df2568667
taruns345 <109971972+taruns345@users.noreply.github.com>
2022-07-25 11:34:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>Tarun amdocsna18 Session </b></p>
+		<p><b>Tarun amdocsna18 Session - Enjoying day 4 training </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 3 </h2> 
